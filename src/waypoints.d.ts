c738c0ce6d55031247de01612c237518a085ede7
Dominik Bulaj <dominik@bulaj.com>
2016-10-18 12:46:27 +0200

    Huge update. Should fully coverage Waypoints API and it's Adapters. Added: # WaypointsGroup's missing axis and waypoints properties # WaypointContext's missing adapters, element and waypoints properties # new WaypointAdapter class declaration # missing Waypoint properties: adapter, context, element, group, options, triggerPoint # missing Waypoint class methods: adapters and Adapter used for Adapter registration (http://imakewebthings.com/waypoints/guides/adapters/#adapter-registration) # adjusted Waypoint.Context.findByElement return type

--- src/waypoints.d.ts
+++ src/waypoints.d.ts
@@ -18,15 +18,44 @@ declare class WaypointGroup {
   public first: () => Waypoint;
   public last: () => Waypoint;
   public name: string;
+  public axis: string;
+  public waypoints: Waypoint[];
 }
 
 declare class WaypointContext {
+  public adapter: WaypointAdapter;
+  public element: HTMLElement|Window;
+  public waypoints: {horizontal: {}, vertical: {}}; // http://imakewebthings.com/waypoints/api/context/#waypoints-property
   public destroy: () => Waypoint;
   public refresh: () => Waypoint;
 }
 
+declare class WaypointAdapter {
+  constructor(element: HTMLElement);
+  public innerHeight(): number;
+  public innerWidth(): number;
+  public off(event: string): undefined;
+  public offset(): {top: number, left: number};
+  public on(event: string, handler: () => void): undefined;
+  public outerHeight(includeMargin: boolean): number;
+  public outerWidth(includeMargin: boolean): number;
+  public scrollLeft(): number;
+  public scrollTop(): number;
+  public static extend(...objects): any;
+  public static inArray(value: any, array: any[], startIndex: number);
+  public static isEmptyObject(object: any): boolean;
+}
+
 declare class Waypoint {
   constructor(options: WaypointOptions);
+  // properties
+  public adapter: WaypointAdapter;
+  public context: WaypointContext;
+  public element: HTMLElement;
+  public group: WaypointGroup;
+  public options: WaypointOptions;
+  public triggerPoint: number;
+
   // Instance Methods
   public destroy(): Waypoint;
   public disable(): Waypoint;
@@ -40,9 +69,13 @@ declare class Waypoint {
   public static refreshAll(): void;
   public static viewportHeight(): number;
   public static viewportWidth(): number;
+  public static adapters: {
+    push: ({name: string, Adapter: WaypointAdapter})
+  };
+  public static Adapter: WaypointAdapter;
   // Waypoint.Context
   public static Context: {
-    findByElement: (element: HTMLElement) => WaypointContext;
+    findByElement: (element: HTMLElement) => WaypointContext|undefined;
   };
   // Waypoint.Group
   public group: WaypointGroup;
